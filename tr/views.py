a3fe38de3b0148643d2efce8663e0b688f67bcf9
anabelle <bellech88@gmail.com>
2019-04-11 19:58:36 -0400

    first commit -- new django project serving html and css for home and welcome pages. also basic database created

---
+++ tr/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+# Create your views here.
+def index(request):
+    return HttpResponse("")
+
+def home(request):
+    return render(request, 'home.html')
+
+def welcome(request):
+    return render(request, 'welcome.html')
