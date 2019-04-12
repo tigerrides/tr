541f22dee4d1c3b7fef67707791b145495301b2a
Christy Lee <christyl@princeton.edu>
2019-04-12 12:15:04 -0400

    connect about and contact pages

--- tr/urls.py
+++ tr/urls.py
@@ -27,5 +27,6 @@ urlpatterns = [
     path('home/', views.home, name='home'),
     path('welcome/', views.welcome, name='welcome'),
     path('admin/', admin.site.urls),
-
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact')
 ]
