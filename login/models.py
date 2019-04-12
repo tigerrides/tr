767aff227fe2160ac8d66fb66ad023358bbb9ccd
anabelle <bellech88@gmail.com>
2019-04-12 11:17:07 -0400

    making a form

--- login/models.py
+++ login/models.py
@@ -4,7 +4,8 @@ from django.db import models
 class LogInInfo(models.Model):
     first_name = models.CharField(max_length=200)
     last_name = models.CharField(max_length=200)
-    phone_number = models.CharField(max_length=10)
+    phone_number = models.IntegerField()
+    img = models.ImageField(default='default.png', blank=True)
 
     def __str__(self):
-        return '%s %s %s' % (self.first_name, self.last_name, self.phone_number)
+        return '%s %s %d' % (self.first_name, self.last_name, self.phone_number)
--- login/views.py
+++ login/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from . import forms
 
 # Create your views here.
 
@@ -7,3 +8,17 @@ def index(request):
 
 def createprof(request):
     return render(request, 'createprof.html')
+
+def profile_create(request):
+
+    if request.method == 'POST':
+        # validate info against form
+        form = forms.CreateProfile(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            # save prof to db
+            return redirect('login')
+    else:
+        form = forms.CreateProfile()
+    return render(request, 'createprof.html', {
+        'form': form})
