403130d635f2001dbc3017a8376c4715be055b24
nschmeller <nbs@princeton.edu>
2019-04-16 23:58:56 -0400

    explicitly defined DATABASES in settings.py

--- tr/settings.py
+++ tr/settings.py
@@ -82,7 +82,9 @@ WSGI_APPLICATION = 'tr.wsgi.application'
 
 DATABASE_URL = os.environ['DATABASE_URL']
 conn = psycopg2.connect(DATABASE_URL, sslmode='require')
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+DATABASES = { 
+        'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
+        }
 
 #DATABASES = {
     #'default': {
