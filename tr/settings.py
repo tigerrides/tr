b3c41a22e68622e5688d0f5b02eba5ec5fb33f6f
nschmeller <nbs@princeton.edu>
2019-04-16 23:12:19 -0400

    updated requirements

--- tr/settings.py
+++ tr/settings.py
@@ -13,10 +13,10 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 import dj_database_url
 import django_heroku
+import psycopg2
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
@@ -80,25 +80,29 @@ WSGI_APPLICATION = 'tr.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
+DATABASE_URL = os.environ['DATABASE_URL']
+conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+
+#DATABASES = {
     #'default': {
     #    'ENGINE': 'django.db.backends.postgresql',
     #    'NAME': os.path.join(BASE_DIR, 'postgres_db'),
     #}
 
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'DATABASE',
-        'USER': '',
-        'PASSWORD': os.environ.get('DB_PASS', ''),
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-}
+ #   'default': {
+  #      'ENGINE': 'django.db.backends.postgresql_psycopg2',
+   #     'NAME': 'DATABASE',
+    #    'USER': '',
+     #   'PASSWORD': os.environ.get('DB_PASS', ''),
+      #  'HOST': 'localhost',
+       # 'PORT': '5432',
+   # }
+#}
 
 # for dj_database_url
-db_from_env = dj_database_url.config()
-DATABASES['default'].update(db_from_env)
+#db_from_env = dj_database_url.config()
+#DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
