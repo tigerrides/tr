cac957e47fa6444439bf00a119778e5fb0d8e600
nschmeller <nbs@princeton.edu>
2019-04-20 22:50:18 -0400

    phone number now formatted as string

--- login/models.py
+++ login/models.py
@@ -4,8 +4,8 @@ from django.db import models
 class LogInInfo(models.Model):
     first_name = models.CharField(max_length=200)
     last_name = models.CharField(max_length=200)
-    phone_number = models.IntegerField()
+    phone_number = models.CharField(max_length=20)
     # img = models.ImageField(default='default.png', blank=True)
 
     def __str__(self):
-        return '%s %s %d' % (self.first_name, self.last_name, self.phone_number)
+        return '%s %s %s' % (self.first_name, self.last_name, self.phone_number)
