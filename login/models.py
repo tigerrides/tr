29a5216f7a13e8c31f885eb991ddf2acd4facf6c
Anabelle <bellech88@gmail.com>
2019-04-22 00:44:03 -0400

    trying phone number field for log in model

--- login/models.py
+++ login/models.py
@@ -1,10 +1,14 @@
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
 
 # Create your models here.
 class LogInInfo(models.Model):
     first_name = models.CharField(max_length=200)
     last_name = models.CharField(max_length=200)
-    phone_number = models.BigIntegerField()
+    phone = PhoneNumberField(null=False, blank=False, unique=True)
+
+
+# phone_number = models.BigIntegerField()
     # img = models.ImageField(default='default.png', blank=True)
 
     def __str__(self):
