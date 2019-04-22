e3517da4723c07d2a45ef961b223500d54fbc855
Anabelle <bellech88@gmail.com>
2019-04-22 01:02:18 -0400

    Revert "revert to char field"
    This reverts commit 260a6f5bb1573c6d43f8569fd2560ee52357e986.

--- login/models.py
+++ login/models.py
@@ -1,10 +1,13 @@
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
 
 # Create your models here.
 class LogInInfo(models.Model):
     first_name = models.CharField(max_length=200)
     last_name = models.CharField(max_length=200)
-    phone_number = models.CharField(max_length=20)
+    phone_number = PhoneNumberField(blank=True)
+
+    # phone_number = models.BigIntegerField()
     # img = models.ImageField(default='default.png', blank=True)
 
     def __str__(self):
