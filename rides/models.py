2a852647a56ba973364d6c0f0acde2776ddc2f1e
Christy Lee <christyl@princeton.edu>
2019-04-21 22:45:20 -0400

    new rides folder

---
+++ rides/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+from model.utils import Choices
+
+# Create your models here.
+class InputRideInfo(models.Model):
+    # ewr = "EWR"
+    # phl = "PHL"
+    # jfk = "JFK"
+    # campus = "PRINCETON"
+    # depart_from = (
+    #     (ewr, "ewr"),
+    #     (phl, "phl"),
+    #     (jfk, "jfk"),
+    #     (campus, "princeton")
+    # )
+    # destination = (
+    #     (ewr, "ewr"),
+    #     (phl, "phl"),
+    #     (jfk, "jfk"),
+    #     (campus, "princeton")
+    # )
+    # drop down menu
+    depart_from = Choices('ewr', 'phl', 'jfk', 'princeton')
+    destination = Choices('ewr', 'phl', 'jfk', 'princeton')
+    date = models.DateField()
+    # not sure what auto_now and auto_now_add means
+    time_start = models.TimeField(auto_now=False, auto_now_add=False)
+    time_end = models.TimeField(auto_now=False, auto_now_add=False)
+    notes = models.CharField(max_length=500)
+    #check boxes
+    uber = models.BooleanField(default=False)
+    lyft = models.BooleanField(default=False)
+    # img = models.ImageField(default='default.png', blank=True)
+
+    # def __str__(self):
+        # return '%s %s %s' % (self.first_name, self.last_name, self.phone_number)
---
+++ rides/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='rides'),
+    path('rides/submit_ride/', views.submit_ride, name='submit_ride'),
+    # path('createprof/profile_create/', views.profile_create, name='profile_create')
+]
