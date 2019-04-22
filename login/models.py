707dfae19d055abc265461a78dbf598728370cd0
Anabelle <bellech88@gmail.com>
2019-04-22 01:07:32 -0400

    removed phone number

--- login/models.py
+++ login/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class LogInInfo(models.Model):
     first_name = models.CharField(max_length=200)
     last_name = models.CharField(max_length=200)
-    phone_number = PhoneNumberField(blank=True)
+    phone_number = models.CharField(max_length=20)
 
     # phone_number = models.BigIntegerField()
     # img = models.ImageField(default='default.png', blank=True)
