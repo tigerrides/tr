50507fa09da8908f07bbb350da722ad32f4bc4b1
Anabelle <bellech88@gmail.com>
2019-04-22 01:06:09 -0400

    remove unnecessary imports

--- login/models.py
+++ login/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from phonenumber_field.modelfields import PhoneNumberField
+# from phonenumber_field.modelfields import PhoneNumberField
 
 # Create your models here.
 class LogInInfo(models.Model):
