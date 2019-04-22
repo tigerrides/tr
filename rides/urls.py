b0861b474629f589e6bf091af212a3726e56e6e7
Christy Lee <christyl@princeton.edu>
2019-04-21 23:06:32 -0400

    fix urls.py name of form

--- rides/urls.py
+++ rides/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='rides'),
+    path('', views.index, name='createRide'),
     path('rides/submit_ride/', views.submit_ride, name='submit_ride'),
     # path('createprof/profile_create/', views.profile_create, name='profile_create')
 ]
