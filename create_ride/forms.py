e6e6fd05883cbe5bc18212525c2018c1cd96a5f8
anabelle <bellech88@gmail.com>
2019-04-23 19:29:21 -0400

    "added forms.py, urls.py, views.py for create_ride dir"

---
+++ create_ride/forms.py
@@ -0,0 +1,18 @@
+from django.forms import ModelForm
+from . import models
+
+
+# Create your models here.
+class CreateRide(ModelForm):
+    class Meta:
+        models = models.InputRideInfo
+        fields = [
+            'depart_from',
+            'destination',
+            'date',
+            'time_start',
+            'time_end',
+            'notes',
+            'uber',
+            'lyft',
+        ]
