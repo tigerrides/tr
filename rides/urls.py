0d787d0cdd96f660f1b77d5f83b5328745a845c6
Anabelle <bellech88@gmail.com>
2019-04-23 20:04:49 -0400

    imported redirect for create_ride model

--- rides/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='createRide'),
-    path('rides/submit_ride/', views.submit_ride, name='submit_ride'),
-    # path('createprof/profile_create/', views.profile_create, name='profile_create')
-]
