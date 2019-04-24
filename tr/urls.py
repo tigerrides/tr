c2265c82c40ff1387102aedcb9cf98e8739f4b99
Christine Kwon <christinekwon22@gmail.com>
2019-04-24 04:32:06 -0400

    changed login urls

--- tr/urls.py
+++ tr/urls.py
@@ -43,4 +43,5 @@ urlpatterns = [
     # path('searchResults/', views.searchResults, name='searchResults'),
     # path(r'^see_rides$', include('create_ride.urls'), name='see_rides'),
 #>>>>>>> 58e80ac679c4a1fe980f2eebabeabb66794c8712
+    path('chooseLogin/accounts/login', include ('login.urls'), name='login'),
 ]
