dac24e289689428789f7b71ab5aa201ce61ca6ee
Christy Lee <christyl@princeton.edu>
2019-04-24 17:33:24 -0400

    add name to accounts urlpatterns in login.urls.py

--- login/urls.py
+++ login/urls.py
@@ -10,5 +10,5 @@ urlpatterns = [
 
 #Add Django site authentication urls (for login, logout, password management)
 urlpatterns += [
-    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('django.contrib.auth.urls'), name='accounts'),
 ]
