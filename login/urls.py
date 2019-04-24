2b7f18de1b379d331cc8ca9c68495678d65d4831
Christy Lee <christyl@princeton.edu>
2019-04-24 17:41:05 -0400

    experiment with commenting out django site authentication urls in login urls.py

--- login/urls.py
+++ login/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
 ]
 
 #Add Django site authentication urls (for login, logout, password management)
-urlpatterns += [
-    path('accounts/', include('django.contrib.auth.urls'), name='accounts'),
-]
+# urlpatterns += [
+#     path('accounts/', include('django.contrib.auth.urls'), name='accounts'),
+# ]
