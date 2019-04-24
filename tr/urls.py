ce710a29fe3a0af33ae06cf29f216d6fd3b4bcb7
Christine Kwon <christinekwon22@gmail.com>
2019-04-24 14:00:45 -0400

    trying to fix create a ride page 4

--- tr/urls.py
+++ tr/urls.py
@@ -38,7 +38,7 @@ urlpatterns = [
     path(r'^see_rides$', include('create_ride.urls'), name='see_rides'),
     path('newride/', views.newRide, name='newRide'),
 #=======
-    path('searchResults/', include('create_ride.urls'), name='searchResults'),
+    path('searchResults/', views.searchResults, name='searchResults'),
 
     # path('searchResults/', views.searchResults, name='searchResults'),
     # path(r'^see_rides$', include('create_ride.urls'), name='see_rides'),
