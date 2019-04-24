0c1a0bd6050d12eb661dc8d152a15622cf4350e2
Christine Kwon <christinekwon22@gmail.com>
2019-04-24 03:50:03 -0400

    temp fix urls 1

--- tr/urls.py
+++ tr/urls.py
@@ -36,7 +36,7 @@ urlpatterns = [
 #<<<<<<< HEAD
     path('searchResults/', views.searchResults, name='searchResults'),
     path(r'^see_rides$', include('create_ride.urls'), name='see_rides'),
-    path('newride/', views.newRide, name='newRide')
+    path('newride/', views.newRide, name='newRide'),
 #=======
     path('searchResults/', include('create_ride.urls'), name='searchResults'),
 
