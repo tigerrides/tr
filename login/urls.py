0ba4e66cc9c88015407a1be1de97ee57390115b2
Christy Lee <christyl@princeton.edu>
2019-04-24 18:11:42 -0400

    Revert "add name to accounts urlpatterns in login.urls.py"
    This reverts commit dac24e289689428789f7b71ab5aa201ce61ca6ee.

--- login/urls.py
+++ login/urls.py
@@ -10,5 +10,5 @@ urlpatterns = [
 
 #Add Django site authentication urls (for login, logout, password management)
 urlpatterns += [
-    path('accounts/', include('django.contrib.auth.urls'), name='accounts'),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
