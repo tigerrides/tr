94b03cd4ed50e51e6532441b12931842acec82ea
Christine Kwon <christinekwon22@gmail.com>
2019-04-24 03:53:46 -0400

    add login urls

--- login/urls.py
+++ login/urls.py
@@ -7,3 +7,8 @@ urlpatterns = [
     path('createprof/', views.createprof, name='createprof'),
     path('createprof/profile_create/', views.profile_create, name='profile_create')
 ]
+
+#Add Django site authentication urls (for login, logout, password management)
+urlpatterns += [
+    path('accounts/', include('django.contrib.auth.urls')),
+]
