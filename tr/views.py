0c51b8748f3e8b2f7f9ceb937f096ee7359fb03d
Christy Lee <christyl@princeton.edu>
2019-04-26 11:44:58 -0400

    comment out login request bc redirects to /admin/logout?

--- tr/views.py
+++ tr/views.py
@@ -11,7 +11,7 @@ def createUser(request):
 		form = UserForm(request.POST)
 		if form.is_valid():
 			new_user = User.objects.create_user(**form.cleaned_data)
-			login(request)
+			# login(request)
 			new_user.save()
 			return render(request, 'createprof.html')
 	else:
