e0e9b293b0a8e7221905775ace7148b840e7d8a0
nschmeller <nbs@princeton.edu>
2019-04-29 12:34:33 -0400

    potential url naming conflict

--- create_ride/urls.py
+++ create_ride/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('', views.rides, name='createRide'),
     # path('searchResults/', include('create_ride.urls'), name='searchResults'),
 
-    path(r'^searchResults/$', views.see_Rides, name='searchResults'),
+    path(r'^searchResults/$', views.see_Rides), #name='searchResults'),
     # path('', views.searchResults, name='searchResults'),
     path('rides/submit_ride/', views.submit_ride, name='submit_ride'),
     # path('createprof/profile_create/', views.profile_create, name='profile_create')
