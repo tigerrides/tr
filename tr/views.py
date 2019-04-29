b84115e8393632cec6496619f50ab367543bbba1
nschmeller <nbs@princeton.edu>
2019-04-29 14:51:23 -0400

    put dict in Context object

--- tr/views.py
+++ tr/views.py
@@ -62,13 +62,13 @@ def rideHistory(request):
 	return render(request, 'rideHistory.html')
 
 def searchResults(request):
-    ride = {}
-    ride['depart_from'] = 'ewr'
-    ride['destination'] = 'princeton'
-    ride['date'] = 'this_date'
-    ride['time_start'] = 'start-time'
-    ride['time_end'] = 'end-time'
-
+    ride_dict = {}
+    ride_dict['depart_from'] = 'ewr'
+    ride_dict['destination'] = 'princeton'
+    ride_dict['date'] = 'this_date'
+    ride_dict['time_start'] = 'start-time'
+    ride_dict['time_end'] = 'end-time'
+    ride = Context(ride_dict)
     return render(request, 'searchResults.html', context=ride)
 
 def newRide(request):
