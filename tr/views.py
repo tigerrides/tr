07060f1bcb936129b3ade27198737da379481e72
nschmeller <nbs@princeton.edu>
2019-04-30 21:49:54 -0400

    i think it throws an error if the database is empty

--- tr/views.py
+++ tr/views.py
@@ -63,6 +63,7 @@ def rideHistory(request):
 	return render(request, 'rideHistory.html')
 
 def searchResults(request):
+    return render(request, 'searchResults.html')
     from django.forms.models import model_to_dict
     submitted_ride = model_to_dict(InputRideInfo.objects.all().latest('created'))
 
