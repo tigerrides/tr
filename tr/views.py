5b507f9464f4798076f1fb8591214696471456f3
nschmeller <nbs@princeton.edu>
2019-04-30 21:52:07 -0400

    back to normal

--- tr/views.py
+++ tr/views.py
@@ -63,7 +63,6 @@ def rideHistory(request):
 	return render(request, 'rideHistory.html')
 
 def searchResults(request):
-    return render(request, 'searchResults.html')
     from django.forms.models import model_to_dict
     submitted_ride = model_to_dict(InputRideInfo.objects.all().latest('created'))
 
