5d784bb2dd4b03b71dd4a9838daa7c171e9169dc
Christine Kwon <christinekwon22@gmail.com>
2019-05-02 18:11:18 -0400

    rating w/ requirements 1

--- tr/settings.py
+++ tr/settings.py
@@ -69,6 +69,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'django.template.context_processors.media',
+                'django.core.context_processors.request',
             ],
         },
     },
