cd3f86ddb7fbd047a01ccc3cf1edb68f626b4539
Christine Kwon <christinekwon22@gmail.com>
2019-05-02 18:49:34 -0400

    ratings commented

--- tr/settings.py
+++ tr/settings.py
@@ -68,16 +68,16 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'django.template.context_processors.media',
+                #'django.template.context_processors.media',
                 #'django.core.context_processors.request',
             ],
         },
     },
 ]
 
-TEMPLATE_CONTEXT_PROCESSORs = [
-    'django.core.context_processors.request',
-]
+#TEMPLATE_CONTEXT_PROCESSORs = [
+#    'django.core.context_processors.request',
+#]
 
 WSGI_APPLICATION = 'tr.wsgi.application'
 
