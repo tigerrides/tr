a2a37c98c050974d64818a05da0a7dbc206f75bf
Anabelle <bellech88@gmail.com>
2019-05-06 15:28:14 -0400

    tyring to get id of a ride

--- create_ride/views.py
+++ create_ride/views.py
@@ -28,20 +28,53 @@ def submit_ride(request):
         # validate info against form
         if not request.user.is_authenticated:
             raise Http404
-        form = forms.CreateRide(request.POST, request.FILES)
-        if form.is_valid():
-            instance = form.save(commit=False)
-            if LogInInfo.objects.filter(user=request.user).exists():
-                get_info = LogInInfo.objects.get(user=request.user)
-                instance.user_first_name = get_info.first_name
-                instance.user_last_name = get_info.last_name
-            instance.user = request.user
-            instance.save()
-            print("saved")
-        # form = forms.CreateProfile(request.POST, request.FILES)
+
+        depart_from = request.POST["depart_from"]
+        destination = request.POST["destination"]
+        date = request.POST["date"]
+        time_start = request.POST["time_start"]
+        time_end = request.POST["time_end"]
+        notes = request.POST["notes"]
+        uber = request.POST.get('uber', False)
+        lyft = request.POST.get('lyft', False)
+
+        if uber != False:
+            uber = True
+
+        if lyft != False:
+            lyft = True
+
+        get_info = LogInInfo.objects.get(user=request.user)
+        if LogInInfo.objects.filter(user=request.user).exists():
+            get_info = LogInInfo.objects.get(user=request.user)
+
+        # phone_number = request.POST["phone_number"]
+        # img = request.POST["img"]
+        input_ride_info = InputRideInfo(user=request.user,
+                                        user_first_name=get_info.first_name,
+                                        user_last_name=get_info.last_name,
+                                        depart_from=depart_from,
+                                        destination=destination,
+                                        date=date,
+                                        time_start=time_start,
+                                        time_end=time_end,
+                                        notes=notes,
+                                        uber=uber,
+                                        lyft=lyft,
+                                        )
+        input_ride_info.save()
+
+        # form = forms.CreateRide(request.POST, request.FILES)
         # if form.is_valid():
-        #     form.save()
-        #     # save prof to db
+        #     instance = form.save(commit=False)
+        #     if LogInInfo.objects.filter(user=request.user).exists():
+        #         get_info = LogInInfo.objects.get(user=request.user)
+        #         instance.user_first_name = get_info.first_name
+        #         instance.user_last_name = get_info.last_name
+        #     instance.user = request.user
+        #     instance.save()
+        #     print("saved")
+
         return redirect('searchResults')
     else:
         form = forms.CreateRide()
