5c2e85f1171a174d171c45987e34fdf92c0e40bb
Christy Lee <christyl@princeton.edu>
2019-05-06 20:41:34 -0400

    fix image 3

--- login/urls.py
+++ login/urls.py
@@ -14,3 +14,7 @@ urlpatterns = [
 urlpatterns += [
     path('accounts/', include('django.contrib.auth.urls')),
 ]
+
+if settings.DEBUG:
+        urlpatterns += static(settings.MEDIA_URL,
+                              document_root=settings.MEDIA_ROOT)
