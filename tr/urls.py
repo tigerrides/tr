ea5b2506a3217e64bc484034311aac8deba57ec8
Anabelle <bellech88@gmail.com>
2019-05-07 00:59:50 -0400

    std urls

--- tr/urls.py
+++ tr/urls.py
@@ -33,14 +33,14 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
     # path('createRide/', views.createRide, name='createRide'),
-    path(r'^groupInfo/(?P<int:ride_id>[0-9]+)$', views.groupInfo, name='groupInfo'),
-           # ''groupInfo/<int:ride_id>',
+    path('groupInfo/', views.groupInfo, name='groupInfo'),
     path('joinGroup/', views.joinGroup, name='joinGroup'),
     path('rideHistory/', views.rideHistory, name='rideHistory'),
 #<<<<<<< HEAD
+    #path('searchResults/', views.searchResults, name='searchResults'),
     path(r'^see_rides$', include('create_ride.urls'), name='see_rides'),
     path('newride/', views.newRide, name='newRide'),
-    path('searchResults/<int:ride_id>/', views.searchResults, name='searchResults'),
+    path('searchResults/<int:ride_id>/', views.searchResults, name='searchResults')
     # path(r'^searchResults/(?P<int:ride_id>[-\w]+)/$', views.searchResults, name='searchResults'),
 
          # 'searchResults/', views.searchResults, name='searchResults'),
