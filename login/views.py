fdd0a3962b67ad08eb23bdb7a15c95e10b9828f9
Christy Lee <christyl@princeton.edu>
2019-05-07 16:58:09 -0400

    try and fix syntax error in login views.py

--- login/views.py
+++ login/views.py
@@ -80,7 +80,7 @@ def cas_profile_create(request):
     created = datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ')
     nonce = ''.join([random.choice('0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ+/=') for i in range(32)])
     username = 'nbs'
-    password = '9194a9a482af7e5bc117c255a7a4d82a'    # use your own from /getkey
+    password = '9194a9a482af7e5bc117c255a7a4d82a'
     generated_digest = b64encode(hashlib.sha256(nonce.encode('utf-8') + created.encode('utf-8') + password.encode('utf-8')).digest()).decode()
     headers = {
         'Authorization': 'WSSE profile="UsernameToken"',
