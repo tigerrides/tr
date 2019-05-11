1d0d25591a12c9bba833c48511fc3b6809827f78
Anabelle <bellech88@gmail.com>
2019-05-11 15:33:31 -0400

    update message

--- tr/views.py
+++ tr/views.py
@@ -191,17 +191,21 @@ def reloadRideHistory(request, which_one):
 		subject = 'TigerRide Trip Update'
 		email_from = settings.EMAIL_HOST_USER
 		recipient_list = []
+		name_phone_num = ""
 		for rides in other_riders:
 			netid = rides['netid']
 			email = netid + '@princeton.edu'
 			recipient_list.append(email)
+			current = InputRideInfo.objects.filter(netid=netid).values()
+			name_phone_num = name_phone_num + netid + ": " + current[phone_num] + "\n"
 			origin = rides['depart_from']
 			destination = rides['destination']
 			date = rides['date']
 		message = 'Dear TigerRider, \n\n' \
 				  '%s %s has left your group for your trip scheduled from %s to %s on %s. \n\n' \
-				  'Safe travels! \n\n' \
-				  'TigerRide' % (me_fn, me_ln, origin, destination, date)
+				  'Safe travels! \n\n' % (me_fn, me_ln, origin, destination, date)
+		message = message + name_phone_num + "\n\nTigerRide"
+
 		send_mail(subject, message, email_from, recipient_list)
 	# delete ride
 	elif which_one == 3:
