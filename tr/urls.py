b1a2256274b08b1943a09fa51b62a8f0d1d768a9
Anabelle <bellech88@gmail.com>
2019-05-11 12:50:14 -0400

    ughghh

--- tr/urls.py
+++ tr/urls.py
@@ -47,6 +47,8 @@ urlpatterns = [
     path(r'^see_rides$', include('create_ride.urls'), name='see_rides'),
     path('newride/', views.newRide, name='newRide'),
     path('searchResults/<int:ride_id>/', views.searchResults, name='searchResults'),
+    path('searchResults/<int:ride_id>/', views.searchResults1, name='searchResults1'),
+
     # path(r'^searchResults/(?P<int:ride_id>[-\w]+)/$', views.searchResults, name='searchResults'),
 
          # 'searchResults/', views.searchResults, name='searchResults'),
