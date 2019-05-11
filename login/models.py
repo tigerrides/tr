e1e16748c88e8140b61992501e96db250b0e6b33
nschmeller <nbs@princeton.edu>
2019-05-11 14:49:32 -0400

    went to manual ratings

--- login/models.py
+++ login/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.conf import settings
-from star_ratings.models import Rating
-from django.contrib.contenttypes.fields import GenericRelation
 
 # Create your models here.
 class LogInInfo(models.Model):
@@ -17,7 +15,8 @@ class LogInInfo(models.Model):
     last_name = models.CharField(max_length=200)
     phone_number = models.CharField(max_length=20)
     image = models.ImageField(upload_to='images/', default="")
-    #rating = GenericRelation(Rating)
+    rating = models.IntegerField(default=5)
+    num_rates = models.IntegerField()
 
     pic_url = models.CharField(max_length=200,
                                default="https://campusplan.princeton.edu/sites/campusplan2/files/styles/pwds_media_xxlarge_no_crop/public/banner-2017-campus-plan-28.jpg?itok=QwrGwh5R")
